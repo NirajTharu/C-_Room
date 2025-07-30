34f9f59ee55163210ed30a3e56c68f401126e70d
NirajTharu <nirajc279@gmail.com>
2025-07-30 08:12:16 +0545

    About the variables in c#

---
+++ Hi_There/Program.cs
@@ -0,0 +1,12 @@
+﻿// First program in C#
+using System;
+namespace Hi_There
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi There!");
+        }
+    }
+}
---
+++ Variables/Program.cs
@@ -0,0 +1,17 @@
+﻿// About the variables in the C#
+// Variables are the containers used to store values 
+
+using System;
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 22;
+            int myRoll = 18;
+            string myName = "Niraj Kumar Chuaudhary.";
+            System.Console.WriteLine("My name is " + myName + " I'am years " + myAge + " years old. " + " My rollNo is " + myRoll);
+        }
+    }
+}
