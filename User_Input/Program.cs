539a7530e2cd6817b7de5925a5980647d8143e3a
NirajTharu <nirajc279@gmail.com>
2025-07-30 19:07:44 +0545

    About the User input in C#

---
+++ User_Input/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// About the user input in the C#
+
+using System;
+namespace User_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username;
+            int age;
+            Console.WriteLine("Enter the User name:");
+            username = Console.ReadLine()!; //If you don't add ! then it will output an error and '!' sign tells the complier don't worry the value in the variable will not be null
+            Console.WriteLine("Enter the age of the user:");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The User name is " + username + " And the age is " + age);
+        }
+    }
+}
