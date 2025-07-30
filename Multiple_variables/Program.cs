fb65a26d354b6a26b4e9d3bff69f35240823718a
NirajTharu <nirajc279@gmail.com>
2025-07-30 08:57:06 +0545

    About the variables and multiple variables

---
+++ Multiple_variables/Program.cs
@@ -0,0 +1,15 @@
+﻿// Multiple varibales in C#
+// It's specially the name through which values are gained.
+using System;
+namespace Multiple_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 22, bank_balance = 150000;
+            string myName = "Pradhan.", blood_G = "O+.";
+            Console.WriteLine("My name is " + myName + "I'am " + age + "years old, " + "And the bank_balance is " + bank_balance + ", blood Group is " + blood_G);
+        }
+    }
+}
--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,6 +1,16 @@
 ﻿// About the variables in the C#
 // Variables are the containers used to store values 
 
+
+
+/*
+-> Names can contain letters, digits and the underscore character (_)
+-> Names must begin with a letter or underscore
+-> Names should start with a lowercase letter, and cannot contain whitespace
+-> Names are case-sensitive ("myVar" and "myvar" are different variables)
+-> Reserved words (like C# keywords, such as int or double) cannot be used as names
+*/
+
 using System;
 namespace Variables
 {
@@ -8,10 +18,18 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            int myAge = 22;
+            int myAge = 2;
             int myRoll = 18;
             string myName = "Niraj Kumar Chuaudhary.";
             System.Console.WriteLine("My name is " + myName + " I'am years " + myAge + " years old. " + " My rollNo is " + myRoll);
+            if (myAge >= 22)
+            {
+                Console.WriteLine("You can Vote!");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Age for voting!");
+            }
         }
     }
 }
