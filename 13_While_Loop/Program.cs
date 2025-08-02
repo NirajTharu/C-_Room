0a4af09cdfc35be7cf4c347144fa98c80f65b0d7
NirajTharu <nirajc279@gmail.com>
2025-08-02 17:19:47 +0545

    About the do  while loop

---
+++ 13_While_Loop/Program.cs
@@ -0,0 +1,31 @@
+﻿// About the while loop in c#
+using System;
+namespace While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int even;
+            int i = 2;
+            Console.WriteLine("Enter the number upto which even number you want to print:");
+            even = Convert.ToInt32(Console.ReadLine());
+            while (i <= even)
+            {
+                Console.WriteLine(i);
+                i += 2;
+            }
+            // This one is do whle loop
+            int a;
+            int j = 0;
+            Console.WriteLine("Enter the number upto which you want to print:");
+            a = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+            while (j <= a);
+        }
+    }
+}
