9984094735a0ee4a7e4b5f8a61da0ae1ebf8ebc9
NirajTharu <nirajc279@gmail.com>
2025-08-02 17:47:55 +0545

    About the for loop

---
+++ 14_For_Loop/Program.cs
@@ -0,0 +1,20 @@
+﻿// About the for loop in C#
+using System;
+namespace For_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nested for loop
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
