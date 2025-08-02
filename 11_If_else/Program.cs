4819f6d5713d93e7918ce8cf44157a4eeddf1b3a
NirajTharu <nirajc279@gmail.com>
2025-08-02 16:47:29 +0545

    About the if else statements

---
+++ 11_If_else/Program.cs
@@ -0,0 +1,23 @@
+﻿// About (Conditional) the if else
+
+using System;
+namespace If_Else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Enter your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            if (age >= 18)
+            {
+                Console.WriteLine("You can Vote!");
+            }
+            else
+            {
+                Console.WriteLine("You can't vote");
+            }
+        }
+    }
+}
