258bba8cd80e793d0db36dcdf32d3eb934aa7bd9
NirajTharu <nirajc279@gmail.com>
2025-08-02 16:36:20 +0545

    About the Bool Data type

---
+++ 10_Bool/Program.cs
@@ -0,0 +1,25 @@
+﻿// About the bool data type which have only two values True/False, On/Off, 1/0
+
+using System;
+namespace Bool_Data_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 9;
+            int b = 11;
+            Console.WriteLine(a > b);
+            Console.WriteLine(a >= b);
+
+            if (a > b)
+            {
+                Console.WriteLine("The value of a and b is: " + $"{a} and {b}");
+            }
+            else
+            {
+                Console.WriteLine("a is not greater than b");
+            }
+        }
+    }
+}
