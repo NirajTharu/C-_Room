583678ecab7c705fc5462a07abd4138d2d176e7a
NirajTharu <nirajc279@gmail.com>
2025-08-03 06:59:32 +0545

    About the foreach loop

---
+++ 15_For_each_loop/Program.cs
@@ -0,0 +1,17 @@
+﻿// About the for each loop 
+using System;
+namespace ForEach_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = ["Jeep", "BYD", "Tata", "Volvo"];
+            foreach (string car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+
+    }
+}
