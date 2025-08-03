7c7b9a44c9b10952f93c460b5b571d8033d99297
NirajTharu <nirajc279@gmail.com>
2025-08-03 06:44:23 +0545

    The small change was done

--- 14_For_Loop/Program.cs
+++ 14_For_Loop/Program.cs
@@ -11,7 +11,7 @@ namespace For_Loop
             {
                 for (int j = 1; j <= i; j++)
                 {
-                    Console.Write("* ");
+                    Console.Write("*");
                 }
                 Console.Write("\n");
             }
