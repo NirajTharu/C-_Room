c4ecea5bf8626f771abd58953eb930aa9f2187a7
NirajTharu <nirajc279@gmail.com>
2025-08-05 06:58:28 +0545

    About the reusable function

---
+++ 19_Methods/Program.cs
@@ -0,0 +1,20 @@
+﻿// About the methods in the c# 
+// Method aslo know as  function takes input or inputs(arguments)
+using System;
+namespace methods
+{
+    class Program
+    {
+        static void Your_name()
+        {
+            string name;
+            Console.WriteLine("Enter Your name:");
+            name = Console.ReadLine();
+            Console.WriteLine($"Your name is: {name}");
+        }
+        static void Main(string[] args)
+        {
+            Your_name();
+        }
+    }
+}
