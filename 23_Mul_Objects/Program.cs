0dda3acebb04eb43a0d8820eb8747f47623ae86a
NirajTharu <nirajc279@gmail.com>
2025-09-17 20:01:05 +0545

    About the multiple classes

---
+++ 23_Mul_Objects/Program.cs
@@ -0,0 +1,18 @@
+﻿// About the multiple objects and classes
+using System;
+namespace Mul_obj_class
+{
+    class Car
+    {
+        public string name = "Spider";
+        public string brand = "Ferrari";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c1 = new Car();
+            Console.WriteLine($"The name of the car  brand is: {c1.brand} and car name is: {c1.name}");
+        }
+    }
+}
