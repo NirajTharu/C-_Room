f2d061dbc3d54680c0375c705318a1c7d6d6c5d4
NirajTharu <nirajc279@gmail.com>
2025-09-17 19:12:32 +0545

    Adding the files about the Method overloading

---
+++ 21_Method_Overloading/Program.cs
@@ -0,0 +1,29 @@
+﻿// Performing the same operation in the different data type
+
+
+
+using System;
+
+namespace Method_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int added_Number = add(99, 101);
+            double added_Number1 = add(33.2343, 343243.3423);
+            Console.WriteLine("The addition of two integer is: " + added_Number);
+            Console.WriteLine("The addition of two double numbers is: " + added_Number1);
+
+        }
+        static int add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static double add(double i, double j)
+        {
+            return i + j;
+        }
+    }
+}
