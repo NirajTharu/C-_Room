1b927490d88d5a4f9ab2ee6051413d0b00e795ae
NirajTharu <nirajc279@gmail.com>
2025-09-17 20:23:22 +0545

    Aout the class members and member functions

---
+++ 24_Class_Members/Program.cs
@@ -0,0 +1,25 @@
+﻿// About the class members and member functions
+// The variables inside the class are fields.
+
+using System;
+namespace Members
+{
+    class P_Name
+    {
+        string Program_Name = "C";
+        string Main_Use = "Building Operating System";
+
+        public void display()
+        {
+            Console.WriteLine($"The Name of the program is: {Program_Name} and Main use case is: {Main_Use}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            P_Name p = new P_Name();
+            p.display();
+        }
+    }
+}
