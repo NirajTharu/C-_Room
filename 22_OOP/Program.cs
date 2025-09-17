db00d9b03429c1279de5b256540c821a4dc6158d
NirajTharu <nirajc279@gmail.com>
2025-09-17 19:38:12 +0545

    Files about the OOP

--- 22_OOP/Program.cs
+++ 22_OOP/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// The OOP(Object Oriented Programming) concept
+// The template for the object is class
+// The instance of class is object
+using System;
+namespace OOP
+{
+    class Car
+    {
+        string name = "V 60";
+        string brand = "volvo";
+
+        static void Main(string[] args)
+        {
+            Car c1 = new Car(); //Created c1 Object of the class Car.
+            Console.WriteLine("The Name of the car is: " + c1.name);
+            Console.WriteLine("The brand of the car is: " + c1.brand);
+        }
+    }
+}
