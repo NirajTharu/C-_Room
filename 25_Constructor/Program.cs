625cfb84fb352c14afd311f584fe516d869b7258
NirajTharu <nirajc279@gmail.com>
2025-09-18 08:05:50 +0545

    Adding the files about the constructor

---
+++ 25_Constructor/Program.cs
@@ -0,0 +1,24 @@
+﻿// Constructor is a special type of the Method that is used to initialze the objects.
+
+using System;
+namespace Constructor
+{
+    class Living_Being
+    {
+        public string name;
+        public string made;
+        public string belongs;
+        public Living_Being(string creature_name, string made_of, string belongs_to)
+        {
+            name = creature_name;
+            made = made_of;
+            belongs = belongs_to;
+        }
+        static void Main(string[] args)
+        {
+            Living_Being l = new Living_Being("Human", "cells", "Mammalia");
+
+            Console.WriteLine(l.name + "is  made of " + l.made + " and belongs to " + l.belongs);
+        }
+    }
+}
