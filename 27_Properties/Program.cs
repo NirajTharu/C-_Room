ebf33cca4af99aba5c1993969967ffe4392e7fb8
NirajTharu <nirajc279@gmail.com>
2025-09-19 20:40:30 +0545

    About the set and get properties in c#

---
+++ 27_Properties/Program.cs
@@ -0,0 +1,19 @@
+﻿// About the get and set properties in C#
+using System;
+namespace properties
+{
+    class Used_Properties
+    {
+        public string Name
+        { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Used_Properties u = new Used_Properties();
+            u.Name = "Niraj Kumar Chaudhary";
+            Console.WriteLine(u.Name);
+        }
+    }
+}
