ab6a6065b68cd4bf809318ce5bef05362c00b356
NirajTharu <nirajc279@gmail.com>
2025-09-21 19:00:45 +0545

    About the Files in C#

---
+++ 35_File/Program.cs
@@ -0,0 +1,17 @@
+﻿// About the file functions in C#
+using System;
+using System.IO;
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string C_sharp = "This is the first file which is about the C#.";
+            File.WriteAllText("FirstFile.txt", C_sharp);
+
+            string view_C_sharp = File.ReadAllText("FirstFile.txt");
+            Console.WriteLine(view_C_sharp);
+        }
+    }
+}
