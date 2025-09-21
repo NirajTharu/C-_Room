7a682269fc21b2d40c279c11773161707672edb1
NirajTharu <nirajc279@gmail.com>
2025-09-21 18:00:54 +0545

    Abot the Enum in the C#

---
+++ 33_Enum/Program.cs
@@ -0,0 +1,24 @@
+﻿// Enum is the special class that represents a grou of constants(unchangeable/read-only variables).
+
+// Enum is hort for "enumerators", which means "Specifically listed".
+
+using System;
+namespace Enum
+{
+    enum List
+    {
+        spoon,
+        plate,
+        cooker,
+        bucket,
+        glass
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List l = List.plate;
+            Console.WriteLine(l);
+        }
+    }
+}
