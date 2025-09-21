c45243f096657e0b1846485021f5b98b55a15e3b
NirajTharu <nirajc279@gmail.com>
2025-09-21 21:01:37 +0545

    About the Exception in the c#

---
+++ 36_Exception/Program.cs
@@ -0,0 +1,25 @@
+﻿// About the exception handling in C#
+// Try and catch;
+using System;
+namespace exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] numbers = { 1, 2, 3 };
+                Console.WriteLine(numbers[4]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("The Try and catch is finished.");
+            }
+        }
+    }
+}
