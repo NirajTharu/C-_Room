248c17075514c424facc60ca67c2a592e8abbbb9
NirajTharu <nirajc279@gmail.com>
2025-09-26 13:00:15 +0545

    Writing in Japanese

---
+++ Konichiwa_World/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Konichiwa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World");
+        }
+    }
+}
